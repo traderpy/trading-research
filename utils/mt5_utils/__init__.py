93b323acc4f5523456b8b7f4fa2d24fbd9a56e98
Tu <juicycactuz@gmail.com>
2023-06-25 23:04:01 +0200

    dax analysis

--- utils/mt5_utils/__init__.py
+++ utils/mt5_utils/__init__.py
@@ -5,12 +5,12 @@ import pandas as pd
 def get_ohlc(symbol, timeframe, start, end):
     bars = mt5.copy_rates_range(symbol, timeframe, start, end)
     bars_df = pd.DataFrame(bars)
-    bars['time'] = pd.to_datetime(bars_df)
+    bars_df['time'] = pd.to_datetime(bars_df['time'], unit='s')
     return bars_df
 
 
 def get_ticks(symbol, start, end):
     bars = mt5.copy_ticks_range(symbol, start, end, mt5.COPY_TICKS_ALL)
     tick_df = pd.DataFrame(bars)
-    tick_df['time'] = pd.to_datetime(tick_df)
+    tick_df['time'] = pd.to_datetime(tick_df['time'], unit='s')
     return tick_df
